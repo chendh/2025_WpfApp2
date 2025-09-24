ac3d65960285e610a97ebfbe232ad2ed2cf021c9
chendh <chendh@office.stust.edu.tw>
2025-09-24 09:58:50 +0800

    調整視窗屬性並新增飲料選項與訂購功能
    修改了 MainWindow.xaml 的視窗高度，新增 Grid 的欄列定義與飲料選項顯示，包括名稱、價格與數量輸入功能。新增訂購按鈕與訂購資訊顯示區域。在 MainWindow.xaml.cs 中新增了數量輸入驗證與點餐資訊顯示的事件處理方法。

--- 2025_WpfApp2/MainWindow.xaml.cs
+++ 2025_WpfApp2/MainWindow.xaml.cs
@@ -20,5 +20,30 @@ namespace _2025_WpfApp2
         {
             InitializeComponent();
         }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var targetTextBox = sender as TextBox;
+            int amount;
+            bool success = int.TryParse(targetTextBox.Text, out amount);
+            if (!success || amount < 0)
+            {
+                MessageBox.Show("請輸入正整數", "輸入錯誤");
+            }
+            else
+            {
+                //MessageBox.Show($"您輸入的數量是 {amount}", "輸入成功");
+                var targetStackPanel = targetTextBox.Parent as StackPanel;
+                var targetNameLabel = targetStackPanel.Children[0] as Label;
+                var targetPriceLabel = targetStackPanel.Children[1] as Label;
+
+                MessageBox.Show($"您點的餐點是 {targetNameLabel.Content}，單價是 {targetPriceLabel.Content}，數量是 {amount}", "輸入成功");
+            }
+        }
     }
 }
