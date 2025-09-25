8a7bbf1368cbe1b9ae57cbb5e05a4e68ced8b9b2
chendh <chendh@office.stust.edu.tw>
2025-09-25 14:31:23 +0800

    更新飲料點餐系統介面與功能
    修改 `MainWindow.xaml` 標題為「飲料點餐系統ver1」，調整介面佈局，新增飲料選項與點餐按鈕。
    新增 `TextBox_TextChanged` 方法，實現數量驗證與總價計算功能，提升使用者體驗。

--- 2025_WpfApp2/MainWindow.xaml.cs
+++ 2025_WpfApp2/MainWindow.xaml.cs
@@ -20,5 +20,24 @@ namespace _2025_WpfApp2
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var targetTextBox = sender as TextBox;
+            var targetStaclkPanel = targetTextBox.Parent as StackPanel;
+            var targetNameLabel = targetStaclkPanel.Children[0] as Label;
+            var targetPriceLabel = targetStaclkPanel.Children[1] as Label;
+
+            int amount;
+            bool success = int.TryParse(targetTextBox.Text, out amount);
+
+            if (!success) MessageBox.Show("請輸入正確的數值", "輸入錯誤");
+            else
+            {
+                string drinkname = targetNameLabel.Content.ToString();
+                int price = Convert.ToInt32(targetPriceLabel.Content.ToString().Substring(0,2));
+                MessageBox.Show($"您選擇的飲料是 {drinkname}，單價是 {price} 元，總共{amount}杯，總價{price * amount}元", "選擇成功");
+            }
+        }
     }
 }
